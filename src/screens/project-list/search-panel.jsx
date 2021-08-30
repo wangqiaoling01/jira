5b9da48d165b53a2fa5ef7d0613a97fa34984232
wangqiaoling <wangqiaoling@MacintoshdeMacBook-Pro.local>
2021-08-30 19:42:34 +0800

    test

--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const SearchPanel = ({users, param, setParam}) => {
 
     return (
-        <form action="">
+        <form>
             <div>
                 <input type="text" value={param.name} onChange={evt=>setParam({
                     ...param,
