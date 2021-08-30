28752c0053b0c44afc2485994be0544c41d66f73
wangqiaoling <wangqiaoling@MacintoshdeMacBook-Pro.local>
2021-08-30 20:55:56 +0800

    chore: 添加 middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { LoginScreen } from "screens/login";
 import "./App.css";
-import { ProjectListScreen } from "./screens/project-list/index";
+// import { ProjectListScreen } from "./screens/project-list/index";
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
